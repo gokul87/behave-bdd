571e0df0a0980076cf59aae5c6419669984fc035
Gokul <gokul.sridharan87@gmail.com>
2018-01-13 01:26:44 +0000

    New version of driver

--- features/browser.py
+++ features/browser.py
@@ -6,7 +6,7 @@ class Browser(object):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("headless")
     chrome_options.add_argument("window-size=1200x600")
-    driver = webdriver.Chrome(executable_path='./drivers/chromedriver_old',
+    driver = webdriver.Chrome(executable_path='./drivers/chromedriver_new',
                               chrome_options=chrome_options)
 
 
